a99f0f1018ca931b4b7ea155c01645c81a267ab1
Mateusz Górzyński <m.gorzynski96@gmail.com>
2018-05-05 13:18:35 +0200

    probowanie wyslania pliku

--- serwer2.c
+++ serwer2.c
@@ -8,6 +8,74 @@
 #include <time.h>
 #include <stdlib.h>
 
+
+void ObsluzPolaczenie(int gn)
+{
+    char sciezka[512];
+    long dl_pliku, wyslano, wyslano_razem, przeczytano;
+    struct stat fileinfo;
+    FILE* plik;
+    unsigned char bufor[1024];
+    
+    memset(sciezka, 0, 512) = "bilety.txt";
+     sciezka = "bilety.txt";
+    if ( sciezka <= 0)
+    {
+        printf("Potomny: blad przy odczycie sciezki\n");
+        return;
+    }
+    
+    printf("Potomny: klient chce plik %s\n", sciezka);
+    
+    if (stat(sciezka, &fileinfo) < 0)
+    {
+        printf("Potomny: nie moge pobrac informacji o pliku\n");
+        return;
+    }
+    
+    if (fileinfo.st_size == 0)
+    {
+        printf("Potomny: rozmiar pliku 0\n");
+        return;
+    }
+    
+    printf("Potomny: dlugosc pliku: %d\n", fileinfo.st_size);
+    
+    dl_pliku = htonl((long) fileinfo.st_size);
+    
+    if (send(gn, &dl_pliku, sizeof(long), 0) != sizeof(long))
+    {
+        printf("Potomny: blad przy wysylaniu wielkosci pliku\n");
+        return;
+    }
+    
+    dl_pliku = fileinfo.st_size;
+    wyslano_razem = 0;
+    plik = fopen(sciezka, "rb");
+    if (plik == NULL)
+    {
+        printf("Potomny: blad przy otwarciu pliku\n");
+        return;
+    }
+    
+    while (wyslano_razem < dl_pliku)
+    {
+        przeczytano = fread(bufor, 1, 1024, plik);
+        wyslano = send(gn, bufor, przeczytano, 0);
+        if (przeczytano != wyslano)
+            break;
+        wyslano_razem += wyslano;
+        printf("Potomny: wyslano %d bajtow\n", wyslano_razem);
+    }
+    
+    if (wyslano_razem == dl_pliku)
+        printf("Potomny: plik wyslany poprawnie\n");
+    else
+        printf("Potomny: blad przy wysylaniu pliku\n");
+    fclose(plik);
+    return;    
+}
+
 int main (void) {
   unsigned int port;
   char bufor[1024], wiadomosc[1024];
@@ -53,13 +121,22 @@ int main (void) {
 
   printf("%s\n","Czekam na połączenie \n");
 
+
   while ((gniazdo2 = accept(gniazdo, (struct sockaddr*) &nadawca, &dl)) > 0) {
     memset(bufor, 0, 1024);
     recv(gniazdo2, bufor, 1024, 0);
     printf("Wiadomość od %s: %s\n", inet_ntoa(nadawca.sin_addr), bufor);
-    sendfile(gniazdo2, f, strlen(wiadomosc), 0);
+    if (fork() == 0)
+        {
+            /* proces potomny */
+            printf("Potomny: zaczynam obsluge\n");
+            ObsluzPolaczenie(gniazdo2);
+            printf("Potomny: zamykam gniazdo\n");
+            close(gniazdo2);
+            printf("Potomny: koncze proces\n");
+            exit(0);
+        }
     printf("%s\n", "Wiadomosc wyslana!\n");
-    close(gniazdo2);
   }
   close(gniazdo);
 
