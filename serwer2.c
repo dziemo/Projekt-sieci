f814b68bddc1a61cb5405b0cf178046390671dbd
Michał Dziemianowicz <michalek210@gmail.com>
2018-05-07 21:35:47 +0200

    odbieranie danych z wieloma klientami

--- serwer2.c
+++ serwer2.c
@@ -9,7 +9,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-void WyslijBilety (int gniazdo) {
+void ObsluzPolaczenie (int gniazdo) {
   char * sciezka = "bilety.txt";
   long dl_pliku, wyslano, wyslano_razem, przeczytano;
   struct stat fileinfo;
@@ -49,33 +49,35 @@ return;
 
   while (wyslano_razem < dl_pliku)
   {
-przeczytano = fread(bufor, 1, 1024, plik);
-wyslano = send(gniazdo, bufor, przeczytano, 0); //sent, buffer, read
-if (przeczytano != wyslano) //read (received) !=sent
-    break;
-wyslano_razem += wyslano;
-printf("Potomny: wyslano %ld bajtow (total sent)\n", wyslano_razem);
+    przeczytano = fread(bufor, 1, 1024, plik);
+    wyslano = send(gniazdo, bufor, przeczytano, 0); //sent, buffer, read
+    if (przeczytano != wyslano) //read (received) !=sent
+      break;
+    wyslano_razem += wyslano;
+    printf("Potomny: wyslano %ld bajtow (total sent)\n", wyslano_razem);
   }
 
   if (wyslano_razem == dl_pliku)
-printf("Potomny: plik wyslany poprawnie (child: file sent ok)\n");
+    printf("Potomny: plik wyslany poprawnie (child: file sent ok)\n");
   else
-printf("Potomny: blad przy wysylaniu pliku (child: file sending error)\n"); //error at se
+    printf("Potomny: blad przy wysylaniu pliku (child: file sending error)\n"); //error at se
   fclose(plik);
   return;
 }
 
 int main (void) {
   unsigned int port;
-  char bufor[1024], wiadomosc[1024];
-  char * miasta[6] = {"Berlin","Warszawa","Madryt","Poznań","Londyn","Moskwa"};
-  int gniazdo, gniazdo2, iloscBiletow;
-  struct sockaddr_in adr, nadawca;
+  char bufor[1024];
+  char * miasta[20] = {"Berlin","Warszawa","Madryt","Poznań","Londyn","Moskwa","Zbąszynek","San Francisco", "Paryż", "Kraków", "Budapeszt", "Rzym", "Sztokholm", "Dublin", "Tallin", "Tokio", "Nowy Jork", "Lizbona", "Ateny", "Wilno"};
+  int gnasluch, gklient, iloscBiletow;
+  long odebrano, rozmDanych;
+  struct sockaddr_in adr;
   socklen_t dl = sizeof(struct sockaddr_in);
 
   size_t len = sizeof(miasta) / sizeof(miasta[0]);
-  printf("%lu\n", len);
-  iloscBiletow =10;
+
+  iloscBiletow = 30;
+
   FILE *f = fopen("bilety.txt", "w");
   if (f == NULL) {
     printf("%s\n", "Nieotwarto pliku");
@@ -86,46 +88,78 @@ int main (void) {
     int idmiasto2 = rand()%len;
     while(idmiasto==idmiasto2)
     {
-      idmiasto2 = miasta[rand()%len];
+      idmiasto2 = rand()%len;
     }
-    fprintf(f, "%s\n", miasta[idmiasto]);
-    fprintf(f, "%s\n", "#");
-    fprintf(f, "%s\n", miasta[idmiasto2]);
-    fprintf(f, "%s\n", "#");
-    fprintf(f, "%u.%u.%u %u:%u\n", rand()%29+1,rand()%11+1,2018,rand()%24,rand()%59);
-    fprintf(f, "%s\n", "#");
-    fprintf(f, "%u:%u\n", rand()%8,rand()%59);
-    fprintf(f, "%s\n", "#");
-    fprintf(f, "R: %u M: %u\n", rand()%16,rand()%8);
-    fprintf(f, "%s\n", "#");
-    fprintf(f, "%u\n", rand()%300+50);
-    fprintf(f, "%s\n", "#");
+    fprintf(f, "%s", miasta[idmiasto]);
+    fprintf(f, "%s", "#");
+    fprintf(f, "%s", miasta[idmiasto2]);
+    fprintf(f, "%s", "#");
+    fprintf(f, "%u.%u.%u %u:%u", rand()%29 + 1,rand()%11+1,2018,rand()%24,rand()%59);
+    fprintf(f, "%s", "#");
+    fprintf(f, "%u:%u", rand()%8,rand()%59);
+    fprintf(f, "%s", "#");
+    fprintf(f, "R: %u M: %u", rand()%15+1,rand()%8+1);
+    fprintf(f, "%s", "#");
+    fprintf(f, "%u", rand()%300+50);
+    fprintf(f, "%s", "#");
   }
   fclose(f);
 
   printf("Podaj port : \n");
   scanf("%u", &port);
-  gniazdo = socket(PF_INET, SOCK_STREAM, 0);
+  gnasluch = socket(PF_INET, SOCK_STREAM, 0);
   adr.sin_family = AF_INET;
   adr.sin_port = htons(port);
   adr.sin_addr.s_addr = INADDR_ANY;
-  if (bind(gniazdo, (struct sockaddr*) &adr, sizeof(adr))<0) {
+  memset(adr.sin_zero, 0, sizeof(adr.sin_zero));
+
+  if (bind(gnasluch, (struct sockaddr*) &adr, sizeof(adr))<0) {
     printf("Bind nie powiódł się.\n");
     return 1;
   }
-  if (listen(gniazdo, 10) < 0) {
-    printf("%s\n", "Listen nie powiódł się.\n");
-    return 1;
-  }
+  listen(gnasluch, 10);
 
-  printf("%s\n","Czekam na połączenie \n");
-
+  while(1)
+  {
+      dl = sizeof(struct sockaddr_in);
+      gklient = accept(gnasluch, (struct sockaddr*) &adr, &dl);
+      if (gklient < 0)
+      {
+          printf("Glowny: accept zwrocil blad\n");
+          continue;
+      }
+      printf("Glowny: polaczenie od %s:%u\n",
+          inet_ntoa(adr.sin_addr),
+          ntohs(adr.sin_port)
+          );
+      printf("Glowny: tworze proces potomny\n");
+      if (fork() == 0)
+      {
+          /* proces potomny */
+          printf("Potomny: zaczynam obsluge\n");
+          ObsluzPolaczenie(gklient);
+          odebrano = recv(gklient, bufor, 1024, 0);
+          FILE *o = fopen("dane.txt", "w");
+          fputs(bufor, o);
+          printf("Potomny: zamykam gniazdo\n");
+          close(gklient);
+          printf("Potomny: koncze proces\n");
+          exit(0);
+      }
+      else
+      {
+          /* proces macierzysty */
+          printf("Glowny: wracam do nasluchu\n");
+          continue;
+      }
+    }
+/*
   while ((gniazdo2 = accept(gniazdo, (struct sockaddr*) &nadawca, &dl)) > 0) {
     WyslijBilety(gniazdo2);
     printf("Wyslano bilety\n");
     close(gniazdo2);
   }
   close(gniazdo);
-
+*/
   return 0;
 }
